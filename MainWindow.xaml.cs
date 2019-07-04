5f0bf2f6a9d5dd6b3a6eaa1836f06887ffb545a4
akshayrkansara <akshayrkansara@gmail.com>
2019-07-04 18:26:06 -0400

    First Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿//Akshay Kansara, Shubham Sharma, Tyler, Anil-->
+//Group 3
+using System;
+using System.Windows;
+namespace Roman_Numeral_Converter
+{
+    public partial class MainWindow : Window
+    {
+        RomanConverter rm = new RomanConverter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //rm.LblOut = "Your Answer";
+        //    DataContext = rm;
+        }
+        private void ConvertToRoman_Click(object sender, RoutedEventArgs e)
+        {
+            String r = TxtBox.Text;
+                switch (r)
+                {
+                    case "1":
+                        r = "I";
+                        lblDisp.Content = r;
+                        break;
+                    case "2":
+                        r = "II";
+                        lblDisp.Content = r;
+                        break;
+                    case "3":
+                        r = "III";
+                        lblDisp.Content = r;
+                        break;
+                    case "4":
+                        r = "IV";
+                        lblDisp.Content = r;
+                        break;
+                    case "5":
+                        r = "V";
+                        lblDisp.Content = r;
+                        break;
+                    case "6":
+                        r = "VI";
+                        lblDisp.Content = r;
+                        break;
+                    case "7":
+                        r = "VII";
+                        lblDisp.Content = r;
+                        break;
+                    case "8":
+                        r = "VIII";
+                        lblDisp.Content = r;
+                        break;
+                    case "9":
+                        r = "IX";
+                        lblDisp.Content = r;
+                        break;
+                    case "10":
+                        r = "X";
+                        lblDisp.Content = r;
+                        break;
+                    default:
+                        lblDisp.Content = "Error Number Out of Range";
+                        break;
+                }
+        }
+
+        
+    }
+}
+
---
+++ RomanConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman_Numeral_Converter
+{
+    class RomanConverter : INotifyPropertyChanged
+    {
+        string lblOut;
+        string txtIn;
+        public string TxtIn
+        {
+            get { return txtIn; }
+            set { txtIn = value; }
+        }
+
+        public string LblOut
+        {
+            get { return lblOut; }
+            set { lblOut = value; propertyChanged(); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void propertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
